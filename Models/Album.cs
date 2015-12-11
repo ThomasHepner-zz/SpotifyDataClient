3210d6cc9706d6ba3eab042789400d7e49b41e2c
Thomas Hepner <thepner32@gmail.com>
2015-12-11 13:12:22 -0300

    Added model

---
+++ Models/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpotifyDataClient.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+        public string Name { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public virtual Artist Artist { get; set; }
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpotifyDataClient.Models
+{
+    public class Song
+    {
+        public int SongID { get; set; }
+        public string Name { get; set; }
+        public float Popularity { get; set; }
+        public float Length { get; set; }
+
+        public virtual Album Album { get; set; }
+    }
+}
