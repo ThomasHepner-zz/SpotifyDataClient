1f70f79f52d141b03b142a8ebf082fb05edf80ec
Thomas Hepner <thepner32@gmail.com>
2015-12-11 21:29:51 -0300

    Data context and configurations

---
+++ DAL/SpotifyContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using SpotifyDataClient.Models;
+
+namespace SpotifyDataClient.DAL
+{
+    public class SpotifyContext : DbContext
+    {
+        public SpotifyContext() : base("SpotifyContext")
+        {
+        }
+
+        public DbSet<Artist> Artists { get; set; } 
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
--- Models/Artist.cs
+++ Models/Artist.cs
@@ -7,9 +7,11 @@ namespace SpotifyDataClient.Models
 {
     public class Artist
     {
-        public int ID { get; set; }
+        public int AlbumID { get; set; }
         public string Name { get; set; }
+        public int ReleaseYear { get; set; }
 
-        public virtual ICollection<Album> Albums { get; set; }
+        public virtual Artist Artist { get; set; }
+        public virtual ICollection<Song> Songs { get; set; }
     }
 }
