4772c461f74bb8893272b45ee1a8f493ac4ade4d
Thomas Hepner <thepner32@gmail.com>
2015-12-13 21:57:20 -0300

    Parsing Spotify's response as JSON

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SpotifyDataClient.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -32,22 +35,42 @@ namespace SpotifyDataClient.Controllers
         public async System.Threading.Tasks.Task<ActionResult> SearchResults(string searchString)
         {
             if (!String.IsNullOrEmpty(searchString)) {
-                searchString = searchString.Replace(' ', '+');
-                Console.WriteLine(searchString);
+                searchString = searchString.TrimEnd(' ').Replace(' ', '+');
             }
+            JObject jsonResponse = null;
+            spotifyCall(jsonResponse, searchString);
+
+            if (jsonResponse == null || (int)jsonResponse["info"]["num_results"] == 0)
+            {
+                ViewBag.Message = "No results found";
+                return View();
+            }
+
+            var artistTracks = jsonResponse["tracks"];
+            int numberOfTracks = (int) jsonResponse["info"]["num_results"] > (int) jsonResponse["info"]["limit"] ? (int) jsonResponse["info"]["limit"] : (int)jsonResponse["info"]["num_results"];
+            for (int i = 0; i < numberOfTracks; i++)
+            {
+                string albumName = (string) artistTracks[i]["album"]["name"];
+                //TODO: save the album as a EF record.
+            }
+            return View();
+        }
+
+        private async void spotifyCall(JObject jsonResponse, string searchString)
+        {
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://ws.spotify.com/");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                HttpResponseMessage response = await client.GetAsync("search/1/track.json?q=" + searchString);
-                if (response.IsSuccessStatusCode)
+                HttpResponseMessage spotifyResponse = await client.GetAsync("search/1/track.json?q=" + searchString);
+                if (spotifyResponse.IsSuccessStatusCode)
                 {
-                    Console.WriteLine("Hola");
+                    var jsonString = await spotifyResponse.Content.ReadAsStringAsync();
+                    jsonResponse = JObject.Parse(jsonString);
                 }
             }
-            return View();
         }
     }
 }
